c4402a3657900d4126ce517f054ca7dd83c985cf
Rafaelov <rafael@xystudio.ru>
2019-02-18 17:27:25 +0300

    --verstka добавление в подвал полоски и ее елементов

--- footer.php
+++ footer.php
@@ -4,7 +4,13 @@
    		<div class="col-6"></div>
    		<div id="footer_bg" class="col-6"></div>
    	</div>
-   	<div id="footer_line"></div>
+   	<div id="footer_line">
+   		<div class="row">
+	   		<div class="col-4"><a href="<?php echo get_template_directory_uri(); ?>/files/agree.pdf" target="__blank">Пользовательское соглашение</a></div>
+	   		<div class="col-4">Все права защищены <?php echo date('Y'); ?></div>
+	   		<div class="col-4"><a href="//xystudio.ru/" target="__blank">Разработано в XYStudio</a></div>
+	   	</div>
+   	</div>
   </footer>
 
 <?php wp_footer(); ?>
