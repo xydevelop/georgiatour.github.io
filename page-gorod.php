5a4bbc5647c98524fa8f81bb00c09f39af9b57f8
Rafaelov <rafael@xystudio.ru>
2019-02-28 17:16:36 +0300

    HTML/CSS

--- page-gorod.php
+++ page-gorod.php
@@ -5,9 +5,25 @@
   	<div class="container">
       <h2>Вид тура</h2>
       <div class="row">
-        <div class="filter-wrapper"></div>
-        <div class="filter-wrapper"></div>
-        <div class="filter-wrapper"></div>
+        <div class="filter-wrapper">
+          <div class="filter-item">Панорама</div>
+          <div class="filter-item">Винный тур</div>
+          <div class="filter-item">На автомобиле</div>
+          <div class="filter-item">Водопады</div>
+        </div>
+        <div class="filter-wrapper">
+          <div class="filter-item">Пеший</div>
+          <div class="filter-item">С проживанием</div>
+          <div class="filter-item">Четыре сезона</div>
+          <div class="filter-item">Крепости</div>
+          <div class="filter-item">Акции</div>
+        </div>
+        <div class="filter-wrapper">
+          <div class="filter-item">Панорама</div>
+          <div class="filter-item">На автомобиле</div>
+          <div class="filter-item">Водопады</div>
+          <div class="filter-item">Панорама</div>
+        </div>
       </div>
     </div>
   </section>
