2687319f3999e0421922037be2fc4b094aedad1e
Rafaelov <rafael@xystudio.ru>
2019-02-28 17:43:06 +0300

    HTML/CSS

--- page-gorod.php
+++ page-gorod.php
@@ -1,10 +1,10 @@
 <?php get_header('page'); ?>
 
 <!-- PAge -->
-  <section class="page">
+  <section class="page city-page">
   	<div class="container">
       <h2>Вид тура</h2>
-      <div class="row">
+      <div class="row text-center">
         <div class="filter-wrapper">
           <div class="filter-item">Панорама</div>
           <div class="filter-item">Винный тур</div>
@@ -27,6 +27,282 @@
       </div>
     </div>
   </section>
+
+   <!-- Single Tour -->
+  <section class="section" id="single_tour">
+    <div class="container">
+      <h2>Однодневные туры</h2>
+      <div class="row">
+         <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour" href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+         <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+       <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="load-more-wrapper text-center">
+          <a class="load-more" href="">Загрузить еще</a>
+        </div>
+    </div>
+  </section>
+    <!-- Multiple Tour -->
+  <section class="section" id="mult_tour">
+    <div class="container">
+      <h2>Многодневные туры</h2>
+      <div class="row">
+       <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+        <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+         <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour"  href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 tour-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/item.png" alt="" />
+            <div>
+              <h4>Пещерный город Плисцихе, горы и Боржоми</h4>
+              <p>Многодневный тур, на автомобиле, с помешением пешер. винный тур, панорамы, отель</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="city">Тбилиси</div>
+                  <div class="time">12 часов (2 дня)</div>
+                </div>
+                <div class="col-md-6 price-wrapper">
+                  <div class="price">5000 р</div>
+                  <span>* за человека</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 pr-0"><div class="review-rating">*****</div><div class="review">( 25 отзывов )</div></div>
+                <div class="col-md-4 pl-0"><a class="btn_tour" href="">Забронировать</a></div>
+              </div>
+            </div>
+          </div>
+      </div>
+       <div class="load-more-wrapper text-center">
+          <a class="load-more" href="">Загрузить еще</a>
+        </div>
+    </div>
+  </section>
+
   
 
 <?php get_footer(); ?>
