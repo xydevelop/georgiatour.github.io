226ae800c08e38c839dbde58c06863dc240a329e
Rafaelov <rafael@xystudio.ru>
2019-03-04 23:35:15 +0300

    HTML/CSS

--- page-tour.php
+++ page-tour.php
@@ -1,7 +1,7 @@
 <?php get_header('page'); ?>
 
 <!-- Tour -->
-  <section class="page tour-page">
+  <section class="page tour-page" style="font: 400 15px Roboto; color: #686868;">
   	<div class="container">
       <div class="row">
         <div class="col-md-6">
@@ -58,21 +58,22 @@
         <div class="col-md-6 places">
           <h4>Места посещения</h4>
           <p>Тбилиси, Мцхета, Казбеги, Сигнаги, Сванетия</p>
-          <div class="gallery-tour-wrapper row">
-            <div class="col-md-6">
+          <div class="gallery-tour-wrapper row align-items-center justify-content-center text-center">
+            <div class="col-md-6 place-img">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/svaneti-item.png" alt="Грузия" />
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 place-img">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/svaneti-item.png" alt="Грузия" />
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 place-img">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/svaneti-item.png" alt="Грузия" />
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 place-img">
                 <img src="<?php echo get_template_directory_uri(); ?>/images/svaneti-item.png" alt="Грузия" />
             </div>
           </div>
         </div>
+        <p>*Экскурсию можно планировать  как из Тбилиси, так же из Кутаиси. Так  же можно планировать по вашему желанию , можно добавить или заменить объекты  посещения.</p>
       </div>
     </div>
   </section>
@@ -95,8 +96,66 @@
       </div>
   </section>
 
-  <section class="tour-guide-info">
+  <section class="tour-guide-image">
     <div class="container"><h4>Краткий обзор тура</h4></div>
+  </section>
+
+   <section class="tour-guide-info">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-2"><span>1</span>день</div>
+        <div class="col-md-10">Прибытие в аэропорт Тбилиси. Трансфер и размещение в гостинице. Прогулка по 
+ночному городу. Ночь в гостинице.</div>
+        <div class="col-md-2"><span>1</span>день</div>
+        <div class="col-md-10">Прибытие в аэропорт Тбилиси. Трансфер и размещение в гостинице. Прогулка по 
+ночному городу. Ночь в гостинице.</div>
+        <div class="col-md-2"><span>1</span>день</div>
+        <div class="col-md-10">Прибытие в аэропорт Тбилиси. Трансфер и размещение в гостинице. Прогулка по 
+ночному городу. Ночь в гостинице.</div>
+      </div>
+    </div>
+  </section>
+
+  <section class="places-should-visit">
+    <div class="container"><h4>Места которые должен посетить каждый</h4></div>
+  </section>
+
+  <section class="places-list">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 place-item p-0">
+          <div class="place-image-wrapper">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/places/martvill.png" alt="" />
+          </div>
+          <div class="place-content">
+            <h4>Мартвильские каньоны</h4>
+            <p>Мартвильские каньоны - Интерес к этой местности вызывает увлекательный километровый каньон с водопадом и недавние находки палеонтологов. По официальным данным, здесь нашли следы обитания первобытного человека и доисторических животных, которые жили на земле 73 миллиона лет назад, а также сохранившиеся следы динозавров и даже их окаменевшие скелеты. Каньоны расположены недалеко от города Зугдиди и пользуются популярностью.  Вы можете прогуляться на гондоле по живописному месту, это более похоже на захватывающее путешествие  или рафтинг.</p>
+            <span class="place-plus">+</span>
+            <span class="close">x</span>
+          </div>
+        </div>
+         <div class="col-md-4 place-item p-0">
+          <div class="place-image-wrapper">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/places/tbilisi.png" />
+          </div>
+          <h4>Город<br/>Тбилиси</h4>
+          <p>столица Грузии и самый большой город.  Архитектура Тбилиси удивит Вас своим разнообразием, узкие улочки с характерными балкончиками, современные мосты , древнейшие церкви и храмы, большое количество кафе и многое другое . Все это создают микс незабываемой атмосферы.  Для осмотра всех его красот Вам понадобится не менее 3 дней.</p>
+          <span class="place-plus">+</span>
+          <span class="close">x</span>
+        </div>
+         <div class="col-md-4 place-item p-0">
+          <div class="place-image-wrapper">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/places/borjomi.png" />
+          </div>
+          <div class="place-content">
+            <h4>Город<br/>Боржоми</h4>
+            <p>Знаменитый город Боржоми- Боржоми — это точно одно из самых популярных названий связанных с Грузией и конечно все ассоциируют его со знаменитой лечебной водой. голубым куполом находится источник с настоящей боржомской водой. Она очень сильно отличается от бутылочной по вкусу, но это истинно натуральная и целебная вода, которую можно пить без ограничений и совершенно бесплатно. Говорят, что для быстрого омолаживания нужно окунуться во всех бассейнах по очередности, начиная с холодного, как в сказке “Конёк — Горбунок”. Кстати, расстояние от входа в парк до бассейнов где-то три километра в одну сторону, что тоже пойдёт во благо, особенно если ходить к ним часто.</p>
+            <span class="place-plus">+</span>
+            <span class="close">x</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 
    
